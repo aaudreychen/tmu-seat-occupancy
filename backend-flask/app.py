fe418c380199c2480adf60db75a333710a69cba2
AndyCa0 <yong.cao@torontomu.ca>
2026-04-08 14:12:38 -0400

    fixed deploy

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -384,7 +384,7 @@ def book_room():
 
 if __name__ == "__main__":
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 5000)),
+        debug=False
     )
